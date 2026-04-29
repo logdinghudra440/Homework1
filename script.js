cb90637f8f9feb5b3e4e78391ef2d478e293bf9c
hudra <logdinghudra@gmail.ru>
2026-04-30 02:32:06 +0600

    задание 2, 3

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ for (let i = 1; i <= 2; i++) {
     console.log("Привет!");  
 }
 
+for (let a = 1; a <= 5; a++) {
+    console.log(a);
+}
+
+for (let b = 0; b <= 22; b++) {
+    if (b < 7) {
+        continue;
+    }
+    console.log(b);
+}
+
